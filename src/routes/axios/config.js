b0e01c14abe72435f9df5d4ce8cc16fea0de68e7
Th <116724740+ThiagoAbreuBR@users.noreply.github.com>
2023-02-02 22:04:25 -0300

    Delete src directory

--- src/routes/axios/config.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Configuração do API Global
-
-const filmesFetch =  axios.create({
-    baseURL : "https://testeapi-xawn.onrender.com",
-})
-
-export default filmesFetch;
